004e485da779161863da0bf0a70b25ccdeb79ad4
Kartik1745 <kartikpatil1997@gmail.com>
2024-03-04 17:21:30 -0500

    Getting all info

--- workday.py
+++ workday.py
@@ -12,43 +12,59 @@ driver = webdriver.Chrome()
 
 wait = WebDriverWait(driver, 10)
 
-driver.get('https://mastercard.wd1.myworkdayjobs.com/en-US/CorporateCareers')
+company_urls = [
+    'https://mastercard.wd1.myworkdayjobs.com/en-US/CorporateCareers', 
+    ]  # Add your company URLs here
 
-today = True
+job_ids_dict = {url: [] for url in company_urls}
 job_urls = []
-while today:
-    time.sleep(1)
-    wait.until(EC.presence_of_element_located((By.XPATH, '//li[@class="css-1q2dra3"]')))
-    
-    job_elements = driver.find_elements(By.XPATH, '//li[@class="css-1q2dra3"]')
 
-    for job_element in job_elements:
-        job_title_element = job_element.find_element(By.XPATH, './/h3/a')
-        job_id_element = job_element.find_element(By.XPATH, './/ul[@data-automation-id="subtitle"]/li')
-        job_id = job_id_element.text
-        posted_on_element = job_element.find_element(By.XPATH, './/dd[@class="css-129m7dg"][preceding-sibling::dt[contains(text(),"posted on")]]')
-        posted_on = posted_on_element.text
-        if 'posted today' in posted_on.lower():
-            job_href = job_title_element.get_attribute('href')
-            job_urls.append(job_href)
-        else:
-            today = False
+for company_url in company_urls:
+    driver.get(company_url)
 
-    next_button = driver.find_element(By.XPATH, '//button[@data-uxi-element-id="next"]')
-    if "disabled" in next_button.get_attribute("class"):
-        break  # exit loop if the "next" button is disabled
-    
-    next_button.click()
+    today = True
+    while today:
+        time.sleep(1)
+        wait.until(EC.presence_of_element_located((By.XPATH, '//li[@class="css-1q2dra3"]')))
+        
+        job_elements = driver.find_elements(By.XPATH, '//li[@class="css-1q2dra3"]')
 
+        for job_element in job_elements:
+            job_title_element = job_element.find_element(By.XPATH, './/h3/a')
+            job_id_element = job_element.find_element(By.XPATH, './/ul[@data-automation-id="subtitle"]/li')
+            job_id = job_id_element.text
+            posted_on_element = job_element.find_element(By.XPATH, './/dd[@class="css-129m7dg"][preceding-sibling::dt[contains(text(),"posted on")]]')
+            posted_on = posted_on_element.text
+            if 'posted today' in posted_on.lower():
+                job_href = job_title_element.get_attribute('href')
+                if job_id not in job_ids_dict[company_url]:
+                    job_ids_dict[company_url].append(job_id)
+                    job_urls.append(job_href)
+            else:
+                today = False
+
+        next_button = driver.find_element(By.XPATH, '//button[@data-uxi-element-id="next"]')
+        if "disabled" in next_button.get_attribute("class"):
+            break  # exit loop if the "next" button is disabled
+        
+        next_button.click()
+
+print(len(job_ids_dict[company_urls[0]]))
 print(len(job_urls))
 
-# jobs = []
+jobs = []
 
-# for job_url in job_urls:
-#     driver.get(job_url)
-#     job_posting_element = wait.until(EC.presence_of_element_located((By.XPATH, '//div[@data-automation-id="jobPostingPage"]')))
-#     job_posting_text = job_posting_element.text
-#     # print(job_posting_text)
-#     driver.back()
+for job_url in job_urls:
+    driver.get(job_url)
+    job_posting_element = wait.until(EC.presence_of_element_located((By.XPATH, '//div[@data-automation-id="jobPostingPage"]')))
+    job_posting_text = job_posting_element.text
+    jobs.append(job_posting_text)
+
+# Write job postings to a CSV file
+with open('job_postings.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['Job Posting'])
+    for job in jobs:
+        writer.writerow([job])
 
 driver.quit()
