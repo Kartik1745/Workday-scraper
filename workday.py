bad31233dbe5ee9d718ca5bd9e5df16f94425142
Kartik1745 <kartikpatil1997@gmail.com>
2024-03-04 15:46:26 -0500

    Location and description fix

--- workday.py
+++ workday.py
@@ -28,27 +28,34 @@ for job_element in job_elements:
     job_title = job_title_element.text
     job_href = job_title_element.get_attribute('href')
     
-    location_element = job_element.find_element(By.XPATH, './/dd[@class="css-129m7dg"]')
-    location = location_element.text
+    # location_element = job_element.find_element(By.XPATH, './/dd[@class="css-129m7dg"]')
+    # location = location_element.text
 
-    job_id_element = job_element.find_element(By.XPATH, './/ul[@data-automation-id="subtitle"]/li')
-    job_id = job_id_element.text
+    # job_id_element = job_element.find_element(By.XPATH, './/ul[@data-automation-id="subtitle"]/li')
+    # job_id = job_id_element.text
     
     posted_on_element = job_element.find_element(By.XPATH, './/dd[@class="css-129m7dg"][preceding-sibling::dt[contains(text(),"posted on")]]')
     posted_on = posted_on_element.text
     
-    if "posted 2 days ago" in posted_on.lower():
+    if "posted today" in posted_on.lower():
         job_title_element.click()
         
         # Wait for the job description to load
-        job_description_element = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'div[data-automation-id="jobPostingDescription"]')))
+        job_description_element = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '[aria-label="Job Posting Description"]')))
         
         # Extract job description
         job_description = job_description_element.text
+
+        locations_elements = job_description_element.find_elements(By.CSS_SELECTOR, "div[data-automation-id='locations'] dd.css-129m7dg")
+        locations = []
+        for location_element in locations_elements:
+            locations.append(location_element.text)
        
         # Append job details to the list
-        jobs.append({'title': job_title,'job-id':job_id ,'href': job_href, 'location': location, 'posted_on': posted_on, 'description': job_description})
+        # jobs.append({'title': job_title,'job-id':job_id ,'href': job_href, 'location': locations, 'posted_on': posted_on, 'description': job_description})
         
+        jobs.append({'title': job_title,'href': job_href, 'location': locations, 'posted_on': posted_on, 'description': job_description})
+
         # Go back to the previous page
         driver.back()
         
